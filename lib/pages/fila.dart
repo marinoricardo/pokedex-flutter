292a6a280e029c9fef368ef306d84676a2c237c8
Marino Ricardo <74591655+marinoricardo@users.noreply.github.com>
2023-01-28 02:03:03 +0200

    criando fileiras

---
+++ lib/pages/fila.dart
@@ -0,0 +1,91 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+
+class Fila extends StatelessWidget {
+  const Fila({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Padding(
+        padding: const EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            Container(
+              height: 200,
+              width: 200,
+              child: Column(
+                children: [
+                  SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: Image.network(
+                        'https://imagepng.org/wp-content/uploads/2017/11/telegram-icone-icon.png'),
+                  ),
+                  Divider(
+                    height: 20,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'data',
+                        style: TextStyle(fontSize: 16),
+                      ),
+                      Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                      )
+                    ],
+                  )
+                ],
+              ),
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.amber,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.red,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.pink,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Container(
+              height: 200,
+              width: 200,
+              color: Colors.blue,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_unnecessary_containers, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:pokedex/pages/fila.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -15,6 +18,17 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Pokedex'),
+        backgroundColor: Colors.red,
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 100,
+            color: Colors.amberAccent,
+            child: Center(child: Text('s')),
+          ),
+          Fila(),
+        ],
       ),
     );
   }
